281f5e2bd0337ddcfea611acfbc0dbc1e30550ed
wallisson-tech <wallissonlopes.ti@gmail.com>
2025-03-24 00:25:56 -0300

    Adiciona o Articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_23_221355) do
-  create_table "products", force: :cascade do |t|
-    t.string "name"
+ActiveRecord::Schema[7.1].define(version: 2025_03_24_030302) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
