5b5c79bcae8ac024803a44bebafaa92c9b00b2b7
wallisson-tech <wallissonlopes.ti@gmail.com>
2025-03-28 15:45:14 -0300

    Adicionando Bootstrap 5

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "@popperjs/core"
+import '../stylesheets/application'
 
 Rails.start()
 Turbolinks.start()
