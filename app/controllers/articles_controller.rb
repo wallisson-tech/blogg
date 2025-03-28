e0085f1c8d829e32c694ba4249fd282a23b9c322
wallisson-tech <wallissonlopes.ti@gmail.com>
2025-03-28 12:23:42 -0300

    Excluindo article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -22,12 +22,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-      @article = Article.find(params[:id])
-
     if @article.update(article_params)
       redirect_to @article
     else
@@ -35,9 +32,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
