c72a2657017022542f6a07d1d096567ff7c77262
wallisson-tech <wallissonlopes.ti@gmail.com>
2025-04-14 21:07:33 -0300

    Adicionando Categorias

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article
+      redirect_to @article, notice: "Article was successfully created."
     else
       render :new
     end
@@ -34,7 +34,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      redirect_to @article
+        redirect_to @article, notice: "Article was successfully updated."
     else
       render :edit
     end
@@ -43,13 +43,13 @@ class ArticlesController < ApplicationController
   def destroy
     @article.destroy
 
-    redirect_to root_path
+    redirect_to root_path, notice: "Article was successfully destroyed."
   end
 
   private
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :category_id)
   end
 
   def set_article
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_24_030302) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_13_123744) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "articles", "categories"
 end
